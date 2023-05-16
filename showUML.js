bde6e844729fb2dad76a1b716e7cd09cff37d322
Yannick Nwankwo <117281275+Yannick-Nw@users.noreply.github.com>
2023-05-16 14:15:53 +0200

    format

--- showUML.js
+++ showUML.js
@@ -1,38 +1,38 @@
-$(document).ready(function() {
-    drawUML();
+$(document).ready(function () {
+	drawUML();
 	downloadUML();
 });
 
 function drawUML() {
-    console.log("ZEICHNEN");
-    $.ajax({
+	console.log("ZEICHNEN");
+	$.ajax({
 		url: "result.json",
 		dataType: "json",
-		success: function(data) {
+		success: function (data) {
 			var classes = data.classes;
 			var html = '<div class="container"><div class="row">';
 			for (var className in classes) {
 				var classData = classes[className];
 				html += '<div class="col-sm">';
 				html += '<div class="card">';
-				html += '<div class="card-header fw-bold">' + className + '</div>';
+				html += '<div class="card-header fw-bold">' + className + "</div>";
 				if (classData.attributes.length > 0) {
 					html += '<div class="card-header">Attributes</div>';
 					html += '<ul class="list-group list-group-flush">';
 					for (var i = 0; i < classData.attributes.length; i++) {
 						var attribute = classData.attributes[i];
 						var attributeType = attribute.charAt(0);
-						var cssClass = '';
-						if (attributeType === '+') {
-							cssClass = 'public-attribute';
-						} else if (attributeType === '#') {
-							cssClass = 'protected-attribute';
-						} else if (attributeType === '-') {
-							cssClass = 'private-attribute';
+						var cssClass = "";
+						if (attributeType === "+") {
+							cssClass = "public-attribute";
+						} else if (attributeType === "#") {
+							cssClass = "protected-attribute";
+						} else if (attributeType === "-") {
+							cssClass = "private-attribute";
 						}
-						html += '<li class="list-group-item ' + cssClass + '">' + attribute + '</li>';
+						html += '<li class="list-group-item ' + cssClass + '">' + attribute + "</li>";
 					}
-					html += '</ul>';
+					html += "</ul>";
 				}
 				if (classData.methods.length > 0) {
 					html += '<div class="card-header">Methods</div>';
@@ -41,51 +41,47 @@ function drawUML() {
 						var method = classData.methods[i];
 						var methodName = method.name;
 						var methodType = methodName.charAt(0);
-						var cssClass = '';
-						if (methodType === '+') {
-							cssClass = 'public-method';
-						} else if (methodType === '#') {
-							cssClass = 'protected-method';
-						} else if (methodType === '-') {
-							cssClass = 'private-method';
+						var cssClass = "";
+						if (methodType === "+") {
+							cssClass = "public-method";
+						} else if (methodType === "#") {
+							cssClass = "protected-method";
+						} else if (methodType === "-") {
+							cssClass = "private-method";
 						}
-						html += '<li class="list-group-item ' + cssClass + '">' + methodName + '</li>';
+						html += '<li class="list-group-item ' + cssClass + '">' + methodName + "</li>";
 					}
-					html += '</ul>';
+					html += "</ul>";
 				}
-				html += '</div>';
-				html += '</div>';
+				html += "</div>";
+				html += "</div>";
 			}
-			html += '</div></div>';
-			$('#uml').html(html);
-			$('.card').draggable();
-		}
-	
+			html += "</div></div>";
+			$("#uml").html(html);
+			$(".card").draggable();
+		},
 	});
-    deleteFiles();
-};
+	deleteFiles();
+}
 
-function deleteFiles(){
-  $.ajax({
-    url: 'deleteFilesFromServer.php',
-    success: function(response) {
-      console.log("Gelöscht");
-    }
-  });
+function deleteFiles() {
+	$.ajax({
+		url: "deleteFilesFromServer.php",
+		success: function (response) {
+			console.log("Gelöscht");
+		},
+	});
 }
 
 function downloadUML() {
-	$('#pic').click(function() {
-        htmlToImage.toJpeg($('#uml')[0], { quality: 0.95 })
-            .then(function (dataUrl) {
-                var link = document.createElement('a');
-                link.download = 'my-image-name.jpeg';
-                link.href = dataUrl;
-                link.click();
-            });
-    });
+	$("#pic").click(function () {
+		htmlToImage.toJpeg($("#uml")[0], { quality: 0.95 }).then(function (dataUrl) {
+			var link = document.createElement("a");
+			link.download = "my-image-name.jpeg";
+			link.href = dataUrl;
+			link.click();
+		});
+	});
 }
 
-function lines() {
-	
-}
+function lines() {}
