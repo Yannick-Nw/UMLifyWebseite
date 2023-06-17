dfdcf9f6c7f67e3209b5ee58370cd0bc9b01f20f
TheRoninski <121442902+TheRoninski@users.noreply.github.com>
2023-06-17 11:49:33 +0200

    edit

--- top_navbar.php
+++ top_navbar.php
@@ -3,7 +3,7 @@ session_start();
 $logged_in = isset($_SESSION["id"]);
 ?>
 <nav class="navbar navbar-expand-md navbar-light bg-light">
-    <a class="navbar-brand" href="index.php">Logo</a>
+    <a class="navbar-brand" href="index.php">Homepage</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
