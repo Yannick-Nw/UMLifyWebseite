bae459dd4d93e4666e7e714c2d3361413839ccca
TheRoninski <121442902+TheRoninski@users.noreply.github.com>
2023-06-18 15:42:36 +0200

    edit

--- register.php
+++ register.php
@@ -31,8 +31,8 @@
                         ?>
                     <form method="POST" action="register.php">
                         
-                        <input id="input-2" name="username" type="text" placeholder="john" required />
-                        <label for="input-2">
+                        <input id="input-1" name="username" type="text" placeholder="john" required />
+                        <label for="input-1">
                             <span class="label-text">Username</span>
                             <span class="nav-dot"></span>
                             <div class="signup-button-trigger">Sign Up</div>
@@ -48,8 +48,8 @@
                                 }
                             }
                             ?>
-                        <input id="input-3" type="email" name="email" placeholder="email@address.com" required />
-                        <label for="input-3">
+                        <input id="input-2" type="email" name="email" placeholder="email@address.com" required />
+                        <label for="input-2">
                             <span class="label-text">Email</span>
                             <span class="nav-dot"></span>
                         </label>
@@ -64,13 +64,13 @@
                                 }
                             }
                             ?>
-                        <input id="input-4" type="text" name="password1" placeholder="&#9679;&#9679;&#9679;DontShareYourPassword&#9679;&#9679;&#9679;" required />
-                        <label for="input-4">
+                        <input id="input-3" type="text" name="password1" placeholder="&#9679;&#9679;&#9679;DontShareYourPassword&#9679;&#9679;&#9679;" required />
+                        <label for="input-3">
                             <span class="label-text">Password</span>
                             <span class="nav-dot"></span>
                         </label>
-                        <input id="input-5" type="text" name="password2" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" required />
-                        <label for="input-5">
+                        <input id="input-4" type="text" name="password2" placeholder="&#9679;&#9679;&#9679;&#9679;&#9679;&#9679;" required />
+                        <label for="input-4">
                             <span class="label-text">Confirm Password</span>
                             <span class="nav-dot"></span>
                         </label>
