bc72675a9c9b88c48c014d28abade9bb8b803337
Yannick Nwankwo <117281275+Yannick-Nw@users.noreply.github.com>
2023-06-26 20:09:34 +0200

    formatieren

--- register.php
+++ register.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Registrierung</title>
@@ -7,30 +8,31 @@
     <link rel="stylesheet" href="css/register.css">
     <script src="animation.js"></script>
 </head>
+
 <body>
-<?php
+    <?php
     include 'top_navbar.php';
     include 'functions.php';
     ?>
     <div class="header">
         <div class="row justify-content-center align-items-center">
             <div class="col-md-6 my-5">
-                    <h1 class="title text-center">Registrierung</h4>
-                        <?php
-                        if (strtoupper($_SERVER['REQUEST_METHOD']) === 'POST') {
-                            $error = false;
-                            $username = $_POST['username'];
-                            $email = $_POST['email'];
-                            $password1 = $_POST['password1'];
-                            $password2 = $_POST['password2'];
-                            if(check_empty() == true){
-                                echo "<p style='color: yellow; font-size: 20px>Bitte füllen Sie alle Felder aus!</p>";
-                                $error = true;
-                            }
-                          }
-                        ?>
+                <h1 class="title text-center">Registrierung</h4>
+                    <?php
+                    if (strtoupper($_SERVER['REQUEST_METHOD']) === 'POST') {
+                        $error = false;
+                        $username = $_POST['username'];
+                        $email = $_POST['email'];
+                        $password1 = $_POST['password1'];
+                        $password2 = $_POST['password2'];
+                        if (check_empty() == true) {
+                            echo "<p style='color: yellow; font-size: 20px>Bitte füllen Sie alle Felder aus!</p>";
+                            $error = true;
+                        }
+                    }
+                    ?>
                     <form method="POST" action="register.php">
-                        
+
                         <input id="input-1" name="username" type="text" placeholder="john" required />
                         <label for="input-1">
                             <span class="label-text">Username</span>
@@ -38,32 +40,32 @@
                             <div class="signup-button-trigger">Sign Up</div>
                         </label>
                         <?php
-                            if(isset($username)){
-                                if(UserReg($username) !== false){
-                                    echo '<p style="color: yellow; font-size: 20px"> Username schon vergeben! </p>';
-                                    $error = true;
-                                }else if(invalidUsername($username) !== false){
-                                    echo '<p style="color: yellow; font-size: 20px">Sonderzeichen sind beim Usernamen nicht erlaubt! </p>';
-                                    $error = true;
-                                }
+                        if (isset($username)) {
+                            if (UserReg($username) !== false) {
+                                echo '<p style="color: yellow; font-size: 20px"> Username schon vergeben! </p>';
+                                $error = true;
+                            } else if (invalidUsername($username) !== false) {
+                                echo '<p style="color: yellow; font-size: 20px">Sonderzeichen sind beim Usernamen nicht erlaubt! </p>';
+                                $error = true;
                             }
-                            ?>
+                        }
+                        ?>
                         <input id="input-2" type="email" name="email" placeholder="email@address.com" required />
                         <label for="input-2">
                             <span class="label-text">Email</span>
                             <span class="nav-dot">Email</span>
                         </label>
                         <?php
-                            if(isset($email)){
-                                if(EmailReg($email) !== false){
-                                    echo '<p style="color: yellow; font-size: 20px"> Email schon vergeben! </p>';
-                                    $error = true;
-                                }else if(invalidEmail($email) !== false){
-                                    echo '<p style="color: yellow; font-size: 20px"> Bitte eine gültige E-Mail-Adresse eingeben! </p>';
-                                    $error = true;
-                                }
+                        if (isset($email)) {
+                            if (EmailReg($email) !== false) {
+                                echo '<p style="color: yellow; font-size: 20px"> Email schon vergeben! </p>';
+                                $error = true;
+                            } else if (invalidEmail($email) !== false) {
+                                echo '<p style="color: yellow; font-size: 20px"> Bitte eine gültige E-Mail-Adresse eingeben! </p>';
+                                $error = true;
                             }
-                            ?>
+                        }
+                        ?>
                         <input id="input-3" type="password" name="password1" placeholder="&#9679;&#9679;DontShareYourPassword&#9679;&#9679;" required />
                         <label for="input-3">
                             <span class="label-text">Password</span>
@@ -75,40 +77,41 @@
                             <span class="nav-dot">Confirm password</span>
                         </label>
                         <?php if ($_SERVER['REQUEST_METHOD'] === 'POST' && $password1 != $password2) { ?>
-                                <p style="color: yellow; font-size: 20px"> Bitte geben Sie zweimal dasselbe Passwort ein. </p>
+                            <p style="color: yellow; font-size: 20px"> Bitte geben Sie zweimal dasselbe Passwort ein. </p>
+                        <?php
+                            $error = true;
+                        }
+                        ?>
+                        <?php
+                        if (isset($_POST["submit"])) {
+                            if (!$error) {
+                                require("dbaccess.php");
+                                //User anlegen
+                                $hash = password_hash($password1, PASSWORD_BCRYPT);
+                                $sql = "INSERT INTO accounts (USERNAME, EMAIL, PASSWORD) VALUES (?,?,?)";
+                                $stmt = mysqli_stmt_init($mysqli);
+                                mysqli_stmt_prepare($stmt, $sql);
+                                mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hash);
+                                mysqli_stmt_execute($stmt);
+                        ?>
+                                <p style="color: green; font-size: 20px"> Dein Account wurde erstellt! </p>
                             <?php
-                                $error = true;
+                                header('Refresh: 1; URL = login.php');
+                                mysqli_stmt_close($stmt);
+                            } else {
+                            ?>
+                                <p style="color: yellow; font-size: 20px"> Registrierung fehlgeschlagen! </p>
+                        <?php
                             }
-                            ?>
-                            <?php
-                            if(isset($_POST["submit"])){
-                                if(!$error){
-                                    require("dbaccess.php");
-                                    //User anlegen
-                                    $hash = password_hash($password1, PASSWORD_BCRYPT);
-                                    $sql = "INSERT INTO accounts (USERNAME, EMAIL, PASSWORD) VALUES (?,?,?)";
-                                    $stmt = mysqli_stmt_init($mysqli);
-                                    mysqli_stmt_prepare($stmt, $sql);
-                                    mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hash);
-                                    mysqli_stmt_execute($stmt);
-                                    ?>
-                                    <p style="color: green; font-size: 20px"> Dein Account wurde erstellt! </p>
-                                    <?php
-                                    header('Refresh: 1; URL = login.php');
-                                    mysqli_stmt_close($stmt);
-                                } else {
-                                    ?>
-                                    <p style="color: yellow; font-size: 20px"> Registrierung fehlgeschlagen! </p>
-                                    <?php
-                                    }
-                                }
-                            ?>
+                        }
+                        ?>
                         <button type="submit" name="submit">Create Your Account</button>
                         <p class="tip">Press Enter to submit</p>
                         <div class="signup-button">Click here to start</div>
                     </form>
-                    </div>
+            </div>
         </div>
     </div>
 </body>
+
 </html>
