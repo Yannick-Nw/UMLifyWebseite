fea5cd4b0824fd80cf216fac1af2b903723e78a9
Yannick Nwankwo <117281275+Yannick-Nw@users.noreply.github.com>
2023-06-27 00:29:03 +0200

    download try 2

--- showUML.js
+++ showUML.js
@@ -79,11 +79,14 @@ function deleteFiles() {
 
 function downloadUML() {
 	$("#pic").click(function () {
-		htmlToImage.toJpeg($("#uml")[0], { quality: 0.95 }).then(function (dataUrl) {
-			var link = document.createElement("a");
-			link.download = "my-image-name.jpeg";
-			link.href = dataUrl;
-			link.click();
+		// htmlToImage.toJpeg($("#uml")[0], { quality: 0.95 }).then(function (dataUrl) {
+		// 	var link = document.createElement("a");
+		// 	link.download = "my-image-name.jpeg";
+		// 	link.href = dataUrl;
+		// 	link.click();
+		// });
+		html2canvas(document.querySelector("#uml")).then(function(canvas) {
+			document.body.appendChild(canvas);
 		});
 	});
 }
